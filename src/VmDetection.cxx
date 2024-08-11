acbc912368c20418d9b0049b9cd5b8f9f375f55e
0xtriboulet <santos.s.salinas@gmail.com>
2024-08-11 16:05:25 -0400

    comment

--- src/VmDetection.cxx
+++ src/VmDetection.cxx
@@ -2,7 +2,7 @@
 
 BOOL VmDetection(float process_count_per_user){
 	
-	
+	// Conditional extracted from DecisionTreeClassifier learnings
 	if ((process_count_per_user > 75.3) || (process_count_per_user > 61.45 && process_count_per_user <= 69.3)){
 		
 		PRINT("[i] Running on bare metal machine!\n");
